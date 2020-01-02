73978034b70e04780434778e59138c5fe67d8e8d
JunaidAshraf <junaid.ssd2418@gmail.com>
2020-01-02 15:00:40 +0530

    test

---
+++ customer_reg_form.php
@@ -0,0 +1,110 @@
+<?php ob_start() ?>
+
+<html>
+<head>
+    <title>Registration Form</title>
+    <link rel="stylesheet" type="text/css" href="css/customer_reg_form.css"/>
+    
+	<?php include'header.php';  ?>
+    </head>
+    <body>
+    <div class="container_regfrm_container_parent">
+	<h3>Online Account Opening Form</h3>
+	<div class="container_regfrm_container_parent_child">
+		<form method="post">
+				 <input type="text" name="name" placeholder="Name" required />
+				 <select name ="gender" required >
+					  <option class="default" value="" disabled selected>Gender</option>
+					  <option value="Male" required >Male</option>
+					  <option value="Female">Female</option>
+					  <option value="Others">Others</option>
+				</select>
+				 <input type="text" name="mobile" placeholder="Mobile no" required />
+				 <input type="text" name="email" placeholder="Email id" />
+				 <input type="text" name="landline" placeholder="Landline no" />
+				 <input type="text" name="dob" placeholder="Date of Birth" onfocus="(this.type='date')" required />
+				 <input type="text" name="pan_no" placeholder="PAN Number" required />
+				 <input type="text" name="aadhar" placeholder="AADHAR Number" required />
+				 <input class="address" type="text" name="homeaddrs" placeholder="Home Address" required  />
+				 <input class="address" type="text" name="officeaddrs" placeholder="Office Address" />
+				 <input type="text" name="country" placeholder="India" value="India" readonly="readonly" />
+
+
+
+				 <select name ="state" required >
+					  <option class="default" value="" disabled selected>State</option>
+					  
+					  <option value="Delhi">Delhi</option>
+					  <option value="Gujrat">Gujrat</option>
+					  <option value="Jharkhand">Jharkhand</option>
+					  <option value="Madhya Pradesh">Madhya Pradesh</option>
+					  <option value="West Bengal">West Bengal</option>
+				</select>
+
+
+
+				 <select name ="city" required >
+					  <option class="default" value="" disabled selected>City</option>
+					  <option value="Kolkata">Kolkata</option>
+					  <option value="Delhi">Delhi</option>
+					  <option value="Surat">Surat</option>
+					  <option value="Darjeeling">Darjeeling</option>
+					  <option value="Ranchi">Ranchi</option>
+					  
+				</select>
+
+
+
+				 
+				 <input type="text" name="pin" placeholder="Pin Code" required />
+				 <input type="text" name="arealoc" placeholder="Area/Locality" required />
+				 <input type="text" name="nominee_name" placeholder="Nominee Name (If any)" />
+				 <input type="text" name="nominee_ac_no" placeholder="Nominee Account no"  />
+				 
+				 <select name ="acctype" required >
+					  <option class="default" value="" disabled selected>Account Type</option>
+					  <option value="Saving">Saving</option>
+					  <option value="Current">Current</option>
+				</select>
+				<input type="submit" name="submit" value="Submit">
+				</form>
+         </div>
+		 </div>
+		 
+<?php include'footer.php';?>
+    
+</body>
+</html>
+
+
+<?php 
+
+if(isset($_POST['submit'])){
+
+	session_start();
+	$_SESSION['$cust_acopening'] = TRUE;
+	$_SESSION['cust_name']=$_POST['name'];
+	$_SESSION['cust_gender']=$_POST['gender'];
+	$_SESSION['cust_mobile']=$_POST['mobile'];
+	$_SESSION['cust_email']=$_POST['email'];
+	$_SESSION['cust_landline']=$_POST['landline'];
+	$_SESSION['cust_dob']=$_POST['dob'];
+	$_SESSION['cust_pan=']=$_POST['pan_no'];
+	$_SESSION['cust_aadhar']=$_POST['aadhar'];
+	$_SESSION['cust_homeaddrs']=$_POST['homeaddrs'];
+	$_SESSION['cust_officeaddrs']=$_POST['officeaddrs'];
+	$_SESSION['cust_country']=$_POST['country'];
+	$_SESSION['cust_state']=$_POST['state'];
+	$_SESSION['cust_city']=$_POST['city'];
+	$_SESSION['cust_pin']=$_POST['pin'];
+	$_SESSION['arealoc']=$_POST['arealoc'];
+	$_SESSION['nominee_name']=$_POST['nominee_name'];
+	$_SESSION['nominee_ac_no']=$_POST['nominee_ac_no'];
+	$_SESSION['cust_acctype']=$_POST['acctype'];
+	
+	header('location:cust_regfrm_confirm.php');
+	
+	
+}
+
+?>
---
+++ footer.php
@@ -0,0 +1,50 @@
+<link rel="stylesheet" type="text/css" href="css/footer.css">
+<footer>
+<div class="container">
+<div class="follow_us">
+    <label>Follow Us</label>
+    <ul>
+        <li><a href="https://www.facebook.com/">Facebook</a></li>
+        <li><a href="https://twitter.com/">Twitter</a></li>
+        <li><a href="https://www.instagram.com/?hl=en">Instagram</a></li>
+    
+    </ul>
+    </div>
+    <div class="contact" id="contactus">
+        <label>Contact Us</label>
+        <ul>
+            <p>Bardhaman- Bankura Road,</p><p>Nischintapur, West Bengal 700137</p>
+            <p>Tel : 033 2482 0676</p>
+            <p> Email: contact@bbit.edu.in</p>
+            
+        </ul>
+    </div>
+        <div class="links">
+            <label>Important Links</label>
+            <ul>    
+            <li><a href="index.php#aboutus" target="_blank">About Us</a></li>
+            <li><a href="https://bankifsccode.com/">Bank IFSC Code</a></li>
+            <li><a href="">Loan</a></li>
+            <li><a href="gst.php">Goods & Services Tax</a></li>
+            <li><a href="">Calculator</a></li> 
+            </ul><br>
+    </div>
+     <div class="About_GST">
+            <label>About GST</label>
+            <ul>    
+            <li><a href="gst.php">GST Council Structure</a></li>
+            <li><a href="gst.php">GST History</a></li>
+            </ul><br>
+    </div>
+    
+    </div>
+    <div class="copyright">
+        <span>Copyright &copy; 2018-19 Student Bank. All rights reserved.</span>
+    </div>
+    
+    <div class="bestview">
+    <span>Site best viewed at 1024 x 768 resolution in Internet Explorer 10+, Google Chrome 49+, Firefox 45+ and Safari 6+</span>
+    
+    </div>
+
+</footer>
